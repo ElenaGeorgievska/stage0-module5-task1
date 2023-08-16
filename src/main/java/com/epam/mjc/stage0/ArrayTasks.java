f444050125f92838e2ef1a5e62a8655b88777a35
ElenaGeorgievska <elena_science@yahoo.com>
2023-08-16 19:29:53 +0200

    stage0_module5_task1

--- src/main/java/com/epam/mjc/stage0/ArrayTasks.java
+++ src/main/java/com/epam/mjc/stage0/ArrayTasks.java
@@ -1,5 +1,10 @@
 package com.epam.mjc.stage0;
 
+
+
+
+
+
 /**
  * Here are the tasks for working with the arrays.
  * <p>
@@ -11,7 +16,9 @@ public class ArrayTasks {
      * Return a String[] array that will list all the seasons of the year, starting with winter.
      */
     public String[] seasonsArray() {
+        String[] array1 = {"Winter", "Spring", "Summer", "Autumn"};
 
+        return array1;
     }
 
     /**
@@ -25,7 +32,25 @@ public class ArrayTasks {
      * length = 5  -> [1, 2, 3, 4, 5]
      */
     public int[] generateNumbers(int length) {
+        // Random rd = new Random(); // creating Random object
+        //int[] arr = new int[10];
+        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr1 = new int[length];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                arr1[i] = arr[i];
+                arr1[i] = length - 1;
+            }
+            // System.out.println(arr[i]); // printing each array element
+        }
 
+
+//        int[] positiveIntegers = {1};
+//        int[] positiveIntegers1 = new int[]{1, 2, 3};
+//        int[] positiveIntegers2 = {1, 2, 3, 4, 5};
+//
+//        return positiveIntegers2;
+        return arr1;
     }
 
     /**
@@ -37,7 +62,11 @@ public class ArrayTasks {
      * arr = [5, -3, -4] -> sum = -2
      */
     public int totalSum(int[] arr) {
-
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        return sum;
     }
 
     /**
@@ -50,7 +79,18 @@ public class ArrayTasks {
      * arr = [5, -3, -4],   number = 10    ->  -1
      */
     public int findIndexOfNumber(int[] arr, int number) {
+        int first = -1;
+        for (int i = 0; i < arr.length; i++) {
 
+            if (arr[i] == number) {
+                first = i;
+                break;
+            }
+//            if (number != arr[i])
+//                continue;
+        }
+        System.out.println(first);
+        return first;
     }
 
     /**
@@ -63,7 +103,24 @@ public class ArrayTasks {
      * arr = ["pineapple", "apple", "pen"] -> ["pen", "apple", "pineapple"]
      */
     public String[] reverseArray(String[] arr) {
+        StringBuilder reverse = new StringBuilder();
+        for (int i = arr.length; i > 0; i--) {
 
+            reverse.append(arr[i - 1]).append(" ");
+            //System.out.println(reverse.append(arr[i-1]).append(" "));
+        }
+        String[] temp = reverse.toString().split(" ");
+
+        return temp;
+
+
+//        String[] arr1 = new String[arr.length];
+//        for (int i = arr.length - 1; i >= 0; i--) {
+//            arr1[i] = arr[i];
+//           System.out.println(arr1[i]);
+//        }
+//        String[] temp = arr1.toString().split(" ");
+//        return temp;
     }
 
     /**
@@ -78,6 +135,22 @@ public class ArrayTasks {
      * arr = [1, 2]         -> [1, 2]
      */
     public int[] getOnlyPositiveNumbers(int[] arr) {
+        int count = 0;
+        for (int n : arr) {
+            if (n > 0) {
+                count++;
+            }
+        }
+        int[] nums = new int[count];
+        int i = 0;
+        for (int n : arr) {
+            if (n > 0) {
+                nums[i] = n;
+                i++;
+            }
+        }
+
+        return nums;
 
     }
 
@@ -93,5 +166,98 @@ public class ArrayTasks {
      */
     public int[][] sortRaggedArray(int[][] arr) {
 
+        for (int i = 0; i < arr.length; i++) {          // rows = arr.length
+            for (int j = 0; j < arr[i].length - 1; j++) {   //columns= arr[i].length
+                for (int k = 0; k < arr[i].length - j - 1; k++) {
+                   //sorting elements in an one dimensional array in ascending order
+                    if (arr[i][k] > arr[i][k + 1]) {
+                        int temp = arr[i][k];
+                        arr[i][k] = arr[i][k + 1];
+                        arr[i][k + 1] = temp;
+                    }
+
+                    //swapping the rows in an array
+//                    int first = arr[j].length;
+//                    int second = arr[j + 1].length;
+//
+//                    if (first > second) {
+//                        //Swap arr[j] and arr[j+1]
+//
+//                        int[] temp2 = arr[j];
+//                        arr[j] = arr[j + 1];
+//                        arr[j + 1] = temp2;
+//                    }
+
+//                        int temp1 = arr[j][k];
+//                        arr[j][k] = arr[j+1][k];
+//                        arr[j+1][k] = temp1;
+
+                    //  System.out.println(first);
+                    // System.out.println(second);
+                    //}
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {          // rows = arr.length
+
+            for (int j = 0; j < arr[i].length - 1; j++) {   //columns= arr[i].length
+
+                    int first = arr[j].length;
+                    int second = arr[j + 1].length;
+
+                    if (first > second) {
+
+                        int[] temp2 = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp2;
+                    }
+
+            }
+        }
+
+
+//        for (int i = 0; i < arr.length - 1; i++) {
+//            for (int j = 0; j < arr.length-i-1; j++) {
+//                for (int k = 0; k < arr[i].length - j - 1; k++) {
+//                    int first = arr[j].length;
+//                    int second = arr[j + 1].length;
+//
+//                    if (first > second) {
+//                        // Swap arr[j] and arr[j+1]
+////                        int temp = first;
+////                        first = second;
+////                        second = temp;
+//
+//                        int temp1 = arr[j][k];
+//                        arr[j][k] = arr[j+1][k];
+//                        arr[j+1][k] = temp1;
+//
+//                      //  System.out.println(first);
+//                       // System.out.println(second);
+//                    }
+//                }
+//            }
+//        }
+
+
+
+        //}
+       // System.out.println(Arrays.deepToString(arr));
+        return arr;
     }
+
+
+
+    public static void main (String[]args){
+        ArrayTasks at = new ArrayTasks();
+//        at.generateNumbers(5);
+        //at.findIndexOfNumber(new int[]{1,2,4,-6,3,6,3,9,90}, 3);
+        // at.reverseArray(new String[]{"pineapple", "apple", "pen"});
+        //at.getOnlyPositiveNumbers(new int[]{1, 2, 3, 4, -5, -6, -7, 8});
+
+
+        at.sortRaggedArray(new int[][]{{5, 6, 4, 3, 1}, {2, 6, 5}});   //new int[] {5, 6, 4, 3, 2},{2,6,5})
+    }
+
 }
